b512dfd72fe2e70323aefc7a2be98999831cb66f
up1 <somkiat.p@gmail.com>
2019-08-12 17:16:08 +0700

    Remove route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { path: '', component: AppComponent, pathMatch: 'full'},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
