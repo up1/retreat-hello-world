9608df34d2219daa98974b43991baad4a106f317
up1 <somkiat.p@gmail.com>
2019-08-12 14:27:01 +0700

    Fix test fail

--- frontend/src/app/shared/rest-api.service.spec.ts
+++ frontend/src/app/shared/rest-api.service.spec.ts
@@ -33,7 +33,7 @@ describe('RestApiService', () => {
     });
 
     // Assert
-    const request = httpTestingController.expectOne('http://localhost:3000/api1');
+    const request = httpTestingController.expectOne('https://fir-860a8.web.app/api1');
     expect(request.request.method).toEqual('GET');
     request.flush(response);
     httpTestingController.verify();
